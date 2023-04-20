91a5e2ad8388485167b2b8fc93485e9e8a559da1
Gustavo Macedo <91705195+gustavomotamacedo@users.noreply.github.com>
2023-04-20 17:43:19 -0300

    namespaces; category endpoits (incomplete)

---
+++ src/Domain/Entity.cs
@@ -0,0 +1,15 @@
+﻿namespace IWantApp.Domain;
+
+public abstract class Entity
+{
+    public Entity()
+    {
+        this.Id = Guid.NewGuid();
+    }
+
+    public Guid Id { get; set; }
+    public string CreatedBy { get; set; }
+    public DateTime CreatedOn { get; set; }
+    public string EditeddBy { get; set; }
+    public DateTime EditeddOn { get; set; }
+}
